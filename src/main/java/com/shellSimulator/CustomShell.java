ef595c16c269d3534a6b935bb3f3b18d1ca89c56
Nischal Joshi <nischaljoshi77@gmail.com>
2025-03-14 19:19:02 -0400

    fix: refactor main method with switch cases.

--- src/main/java/com/shellSimulator/CustomShell.java
+++ src/main/java/com/shellSimulator/CustomShell.java
@@ -3,10 +3,10 @@ package com.shellSimulator;
 import java.util.*;
 
 public class CustomShell {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ShellCommand shellCommand = new ShellCommand();
+        
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine().trim();
@@ -16,40 +16,55 @@ public class CustomShell {
             String command = tokens[0];
             String[] arguments = Arrays.copyOfRange(tokens, 1, tokens.length);
 
-            if (command.equals("exit")) {
-                System.out.println("Exiting shell...");
-                break;
-            } else if (command.equals("pwd")) {
-                System.out.println(System.getProperty("user.dir"));
-            } else if (command.equals("cd")) {
-                shellCommand.changeDirectory(arguments);
-            } else if (command.equals("echo")) {
-                System.out.println(String.join(" ", arguments));
-            } else if (command.equals("clear")) {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-            } else if (command.equals("ls")) {
-                shellCommand.listFiles();
-            } else if (command.equals("mkdir")) {
-                shellCommand.createDirectory(arguments);
-            } else if (command.equals("rmdir")) {
-                shellCommand.removeDirectory(arguments);
-            } else if (command.equals("rm")) {
-                shellCommand.removeFile(arguments);
-            } else if (command.equals("touch")) {
-                shellCommand.touchFile(arguments);
-            } else if (command.equals("jobs")) {
-                shellCommand.listJobs();
-            } else if (command.equals("kill")) {
-                shellCommand.killProcess(arguments);
-            } else if (command.equals("fg")) {
-                shellCommand.bringToForeground(arguments);
-            } else if (command.equals("bg")) {
-                shellCommand.resumeInBackground(arguments);
-            } else {
-                shellCommand.executeCommand(tokens, false); // Default: run in foreground
+            switch (command) {
+                case "exit":
+                    System.out.println("Exiting shell...");
+                    scanner.close();
+                    return;
+                case "pwd":
+                    System.out.println(System.getProperty("user.dir"));
+                    break;
+                case "cd":
+                    shellCommand.changeDirectory(arguments);
+                    break;
+                case "echo":
+                    System.out.println(String.join(" ", arguments));
+                    break;
+                case "clear":
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    break;
+                case "ls":
+                    shellCommand.listFiles();
+                    break;
+                case "mkdir":
+                    shellCommand.createDirectory(arguments);
+                    break;
+                case "rmdir":
+                    shellCommand.removeDirectory(arguments);
+                    break;
+                case "rm":
+                    shellCommand.removeFile(arguments);
+                    break;
+                case "touch":
+                    shellCommand.touchFile(arguments);
+                    break;
+                case "jobs":
+                    shellCommand.listJobs();
+                    break;
+                case "kill":
+                    shellCommand.killProcess(arguments);
+                    break;
+                case "fg":
+                    shellCommand.bringToForeground(arguments);
+                    break;
+                case "bg":
+                    shellCommand.resumeInBackground(arguments);
+                    break;
+                default:
+                    shellCommand.executeCommand(tokens, false);
+                    break;
             }
         }
-        scanner.close();
     }
 }
