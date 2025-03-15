0962042a715da938818b151e2fe3f8d9e8353d78
rutus-code <rutushah105@gmail.com>
2025-03-15 09:49:09 -0400

    modified the customshell class for input of roundrobin and priorityscheduling algorithmn git push

--- src/main/java/com/shellSimulator/CustomShell.java
+++ src/main/java/com/shellSimulator/CustomShell.java
@@ -71,26 +71,26 @@ public class CustomShell {
                     int numProcesses = Integer.parseInt(scanner.nextLine());
                     List<SchedulingProcess> processes = new ArrayList<>();
                     for (int i = 1; i <= numProcesses; i++) {
-                        System.out.print("Enter burst time for Process " + i + ": ");
-                        int burstTime = Integer.parseInt(scanner.nextLine());
                         System.out.print("Enter arrival time for Process " + i + ": ");
                         int arrivalTime = Integer.parseInt(scanner.nextLine());
+                        System.out.print("Enter burst time for Process " + i + ": ");
+                        int burstTime = Integer.parseInt(scanner.nextLine());
                         processes.add(new SchedulingProcess(i, 0, burstTime, arrivalTime)); // Priority is 0 for Round-Robin
                     }
 
                     scheduler.roundRobinScheduling(processes, timeSlice);
                     break;
-                case "priorityScheduling":
+                case "priorityscheduling":
                     System.out.print("Enter number of processes: ");
                     numProcesses = Integer.parseInt(scanner.nextLine());
                     processes = new ArrayList<>();
                     for (int i = 1; i <= numProcesses; i++) {
+                        System.out.print("Enter arrival time for Process " + i + ": ");
+                        int arrivalTime = Integer.parseInt(scanner.nextLine());
                         System.out.print("Enter burst time for Process " + i + ": ");
                         int burstTime = Integer.parseInt(scanner.nextLine());
                         System.out.print("Enter priority for Process " + i + ": ");
                         int priority = Integer.parseInt(scanner.nextLine());
-                        System.out.print("Enter arrival time for Process " + i + ": ");
-                        int arrivalTime = Integer.parseInt(scanner.nextLine());
                         processes.add(new SchedulingProcess(i, priority, burstTime, arrivalTime));
                     }
 
